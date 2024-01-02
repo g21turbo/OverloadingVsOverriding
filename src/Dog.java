3b7651028c26c0ded45b12d687fa80ad08fea516
g21turbo <dg21stew@gmail.com>
2024-01-01 18:38:00 -0800

    Initial commit

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog {
+
+
+    public void bark() {
+        System.out.println("Woof");
+    }
+
+    public void bark(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.println("Woof");
+        }
+    }
+}
+
---
+++ src/GermanShepherd.java
@@ -0,0 +1,8 @@
+public class GermanShepherd extends Dog {
+
+    @Override
+    public void bark() {
+        System.out.println("Woof Woof Woof");
+    }
+
+}
